1a1f2ba77f6e6188fb2ed2ff810095b12c07c81b
Nina Petit <nina.petit6@gmail.com>
2023-04-02 19:02:24 +0200

    Beginning of hero section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import header_picture from './assets/hero_picture.JPG';
+import { ReactComponent as BubbleIcon } from './assets/icons/bubble.svg';
+import { ReactComponent as OpenIcon } from './assets/icons/open.svg';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="hero-section">
+        <div className="hero-section__left">
+          <div className="hero-section__info">
+            <h1>NINA <span style={{fontWeight: "800"}}>PETIT</span></h1>
+            <h2>Développeuse web et mobile</h2>
+            <p className="hero-section__info__presentation">
+              Je suis passionnée par le développement de sites web et
+              d’applications mobiles, et je recherche actuellement un emploi en
+              CDI en full remote.
+            </p>
+            <div className="hero-section__info__buttons">
+              <div className="hero-section__info__buttons__contact">
+                <p>Contactez-moi</p>
+                <BubbleIcon className="hero-section__info__buttons__contact__icon"/>
+              </div>
+              <div className="hero-section__info__buttons__cv">
+                <p>
+                  Ouvrir mon CV
+                </p>
+                <OpenIcon className="hero-section__info__buttons__cv__icon"/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img src={header_picture} alt="me" className="hero-section__img" />
+      </div>
     </div>
   );
 }
